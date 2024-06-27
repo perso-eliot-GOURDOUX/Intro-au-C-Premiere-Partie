a39c3c1bc6f17291f6fe03d2ba9cbf5093e6f90a
Eliot GOURDOUX <eliot.gourdoux@epitech.eu>
2024-06-27 22:13:03 +0200

    fix: fix strucuture of class rectangle in ex 10

--- ex09.cpp
+++ ex09.cpp
@@ -1,21 +1,18 @@
 #include <iostream>
-#include <string>  // Inclure la bibliothèque pour utiliser std::string
+#include <string>
 
 using namespace std;
 
-// Définition de la structure Personne
 struct Personne {
     string nom;
     int age;
 };
 
 int main() {
-    // Déclaration et initialisation d'une variable de type Personne
     Personne personne1;
     personne1.nom = "Jean";
     personne1.age = 30;
 
-    // Affichage des valeurs des membres de la structure
     cout << "Nom de la personne : " << personne1.nom << endl;
     cout << "Âge de la personne : " << personne1.age << " ans" << endl;
 
--- ex10.cpp
+++ ex10.cpp
@@ -3,42 +3,35 @@
 
 using namespace std;
 
-// Définition de la classe Rectangle
 class Rectangle {
-private:
-    const string name;
-    double longueur;
-    double largeur;
+    public:
+        Rectangle() {
+            longueur = 0.0;
+            largeur = 0.0;
+        }
 
-public:
-    // Constructeur par défaut
-    Rectangle() {
-        longueur = 0.0;
-        largeur = 0.0;
-    }
+        Rectangle(string name, double l, double w) : name(name), longueur(l), largeur(w) {}
 
-    // Constructeur avec paramètres
-    Rectangle(string name, double l, double w) : name(name), longueur(l), largeur(w) {}
+        double calculerAire() {
+            return longueur * largeur;
+        }
 
-    // Méthode pour calculer l'aire du rectangle
-    double calculerAire() {
-        return longueur * largeur;
-    }
+        double calculerPerimetre() {
+            return 2 * (longueur + largeur);
+        }
+        string getName() const {
+            return name;
+        }
 
-    // Méthode pour calculer le périmètre du rectangle
-    double calculerPerimetre() {
-        return 2 * (longueur + largeur);
-    }
-    string getName() const {
-        return name;
-    }
+    private:
+        const string name;
+        double longueur;
+        double largeur;
 };
 
 int main() {
-    // Création d'un objet de la classe Rectangle
     Rectangle monRectangle("Jack", 5.0, 3.0);
 
-    // Calcul et affichage de l'aire et du périmètre
     cout << "Nom du rectangle : " << monRectangle.getName() << endl;
     cout << "Aire du rectangle : " << monRectangle.calculerAire() << endl;
     cout << "Périmètre du rectangle : " << monRectangle.calculerPerimetre() << endl;
